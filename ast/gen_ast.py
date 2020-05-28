32ae454036b6d2b6cc61b38ec972707a3376b376
MrConnorKenway <auriaiur@outlook.com>
2020-05-28 12:26:31 +0800

    Add member function support when generating ast src and/or header files
    - and optimize regular expression

--- ast/gen_ast.py
+++ ast/gen_ast.py
@@ -24,27 +24,32 @@ def main(src, header, tmpl):
   src.write('#include "build/include/ast.h"\n#include "build/include/visitor.h"\n\n')
 
   for subclass_name in subclass_names:
-    fields_decl = re.findall(
-        r"class {0}.*\n^.*public:.*\n\s*([\s\S]*?)}};".format(subclass_name), tmpl, re.MULTILINE)
+    members_decl = re.findall(
+        r"class {0}.*\n^.*public:.*\n\s*([\s\S]*?)\n(\n[\s\S]*?)?}};".format(subclass_name), tmpl, re.MULTILINE)
+    if (len(members_decl) != 0):
+      # use zip in conjunction with the * operator to unzip declarations
+      fields_decl, funcs_decl = zip(*members_decl)
+    else:
+      fields_decl, funcs_decl = [], []
 
+    header.write(
+        'class {0} : public AST_node_base {{\n public:\n\t'.format(subclass_name))
     if len(fields_decl) == 0:
       # if the class does not have any fields
       header.write(
-          'class {0} : public AST_node_base {{\n public:\n\t/*virtual*/ void accept(Visitor& v);\n\t{0}();\n}};\n\n'.format(subclass_name))
+          '/*virtual*/ void accept(Visitor& v);\n\t{0}();\n'.format(subclass_name))
       src.write(
           'void {0}::accept(Visitor& v) {{ v.visit(this); }}\n{0}::{0}() {{ node_type = "{0}"; }}\n\n'.format(subclass_name))
     else:
-      fields = re.findall(r"([\s\S]*?)\n", fields_decl[0], re.MULTILINE)
+      fields = re.findall(
+          r"\s*(\S+)\s*(\S+);(?:\s*//\s*(.*))?\n?", fields_decl[0], re.M)
       parameter_list_str = ''
       assignment_list_str = ''
       is_first = True
       for field in fields:
-        field = field.replace(';', '')
-        if field.count('//') > 0:
+        type_name, id_name, init_value = field
+        if init_value != '':
           # if we specify the initial value of this field
-          field, init_value = field.split('//')
-          type_name, id_name = field.split()
-          init_value = init_value.split()[0]
           if init_value == 'default':
             init_value = ''
           if is_first:
@@ -53,7 +58,6 @@ def main(src, header, tmpl):
           else:
             assignment_list_str += ', {0}({1})'.format(id_name, init_value)
         else:
-          type_name, id_name = field.split()
           if is_first:
             parameter_list_str += '{0} _{1}'.format(type_name, id_name)
             assignment_list_str += '{0}(_{0})'.format(id_name)
@@ -62,10 +66,14 @@ def main(src, header, tmpl):
             parameter_list_str += ', {0} _{1}'.format(type_name, id_name)
             assignment_list_str += ', {0}(_{0})'.format(id_name)
       header.write(
-          'class {0} : public AST_node_base {{\n public:\n\t{3}\n\t/*virtual*/ void accept(Visitor& v);\n\t{0}({1});\n}};\n\n'.format(
+          '{3}\n\t/*virtual*/ void accept(Visitor& v);\n\t{0}({1});\n'.format(
               subclass_name, parameter_list_str, assignment_list_str, fields_decl[0]))
       src.write(
           'void {0}::accept(Visitor& v) {{ v.visit(this); }}\n{0}::{0}({1}) : {2} {{ node_type = "{0}"; }}\n\n'.format(subclass_name, parameter_list_str, assignment_list_str, fields_decl[0]))
+    for func in funcs_decl:
+      if func != '':
+        header.write('{0}'.format(func))
+    header.write('};\n\n')
 
 
 if __name__ == "__main__":
