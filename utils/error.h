812d7103aacdc19ea596ac24e91e420c19c76d0d
MrConnorKenway <auriaiur@outlook.com>
2020-05-28 21:53:50 +0800

    Use variadic templates ane function template specialization to support fine-grained control of output

--- utils/error.h
+++ utils/error.h
@@ -5,17 +5,53 @@
 extern YYLTYPE* yylloc_ptr;
 extern string src_file_name;
 
-#define ss_assert(expr, args...)                                            \
-  if (!(expr)) {                                                            \
+#define ss_assert(expr, args...)                                          \
+  if (!(expr)) {                                                          \
     fprintf(stderr, "Compiling failed at:%s:%d:%s\n", __FILE__, __LINE__, \
-            __PRETTY_FUNCTION__);                                           \
-    ss_assert_helper(args);                                                 \
-    exit(-1);                                                               \
+            __PRETTY_FUNCTION__);                                         \
+    cerr << src_file_name << ":" << yylloc_ptr->first_line << ":"         \
+         << yylloc_ptr->first_column << " ";                              \
+    ss_assert_helper(args);                                               \
+    cerr << endl;                                                         \
+    exit(-1);                                                             \
   }
 
+template <typename T, typename... Args>
+inline void ss_assert_helper(T&& t, Args&&... args);
+
 template <typename... Args>
-inline void ss_assert_helper(Args&&... args) {
-  (cerr << src_file_name << ":" << yylloc_ptr->first_line << ":"
-        << yylloc_ptr->first_column << " " << ... << args)
-      << endl;  // since c++ 17
+inline void ss_assert_helper(string s, Args&&... args);
+
+template <typename... Args>
+inline void ss_assert_helper();
+
+template <typename... Args>
+inline void ss_assert_helper() {}
+
+template <typename T, typename... Args>
+inline void ss_assert_helper(T&& t);
+
+template <typename... Args>
+inline void ss_assert_helper(string s);
+
+template <typename T, typename... Args>
+inline void ss_assert_helper(T&& t) {
+  cerr << t;
+}
+
+template <typename... Args>
+inline void ss_assert_helper(string s) {
+  cerr << "\"" << s << "\"";
+}
+
+template <typename T, typename... Args>
+inline void ss_assert_helper(T&& t, Args&&... args) {
+  cerr << t;
+  ss_assert_helper(std::forward<Args>(args)...);
+}
+
+template <typename... Args>
+inline void ss_assert_helper(string s, Args&&... args) {
+  cerr << "\"" << s << "\"";
+  ss_assert_helper(std::forward<Args>(args)...);
 }
